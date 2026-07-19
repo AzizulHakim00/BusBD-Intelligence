2b4be95ac282c095f4eb6983567c8c5b05e0a8ca
Azizul Hakim Omor <157488702+AzizulHakim00@users.noreply.github.com>
2026-07-19 09:28:22 +0600

    Mount V2.3 portal without replacing original frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import V23Portal from './V23Portal'
 import './fonts.css'
 import './styles.css'
 import './v1.css'
+import './v23.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+    <V23Portal />
+  </React.StrictMode>
+)
