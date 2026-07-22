33d245854e77c143ed2c56b87e3060e596d0edc7
Azizul Hakim Omor <157488702+AzizulHakim00@users.noreply.github.com>
2026-07-22 07:30:52 +0600

    Load functional HAR UI completion styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './fonts.css'
 import './styles.css'
+import './app-overrides.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
