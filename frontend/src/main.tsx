aaa54b5a9017ecc252bbf541862492e4cedc3c76
Azizul Hakim Omor <157488702+AzizulHakim00@users.noreply.github.com>
2026-07-22 08:05:27 +0600

    Mount the live journey experience layer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ExperienceLayer from './experience/ExperienceLayer'
 import './fonts.css'
 import './styles.css'
 import './app-overrides.css'
@@ -8,5 +9,6 @@ import './app-overrides.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <ExperienceLayer />
   </React.StrictMode>
 )
